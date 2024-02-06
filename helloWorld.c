9425c07b6fa90bb4bf51a9f5551c4cb27fa767ac
kindanotright <143106706+kindanotright@users.noreply.github.com>
2024-02-06 14:58:51 -0600

    Create helloWorld.c

---
+++ helloWorld.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Megan Bogatz
+ *         mbogatz2@huskers.unl.edu
+ * Date: 2023/08/19
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!");
+
+  return 0;
+}
