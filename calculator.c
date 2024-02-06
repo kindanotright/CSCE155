86286ac79f898e8bd51e82c77a179c479b96309f
kindanotright <143106706+kindanotright@users.noreply.github.com>
2024-02-06 15:02:39 -0600

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,67 @@
+/**
+ * Author:
+ * Date:
+ *
+ * This programs provides basic calculator functionality
+ * allowing a user to enter two operands and to compute
+ * various calculated values.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+ 
+int main(int argc, char **argv) {
+ 
+  double a, b, result;
+  int choice;
+ 
+  printf("Enter operand a: ");
+  scanf("%lf", &a);
+ 
+  printf("Enter operand b: ");
+  scanf("%lf", &b);
+ 
+  printf("Enter operation:\n");
+  printf("(1) Addition\n");
+  printf("(2) Subtraction\n");
+  printf("(3) Multiplication\n");
+  printf("(4) Division\n");
+  printf("(5) Minimum\n");
+  printf("(6) log_a(b)\n");
+  scanf("%d", &choice);
+ 
+  if(choice == 1) {
+    printf("%f", a + b);
+  } else if(choice == 2) {
+    result = a - b;
+    printf("%f", result);
+  } else if(choice == 3) {
+    result = a * b;
+    printf("%lf", result);
+  } else if(choice == 4) {
+    if (b == 0){
+      printf("ERROR\n");
+    } else if (b < 0 || b > 0) {
+      result = a / b;
+      printf ("%lf", result);
+    }
+  } else if(choice == 5) {
+    if (b < a){
+      printf ("%lf", b);
+    } else if (a < b) {
+      printf ("%lf", a);
+    }
+  } else if(choice == 6) {
+    if (a <= 0 || b <= 0){
+      printf ("ERROR\n");
+    } else if (a > 0 || b > 0){
+      result = log(b) / log(a);
+      printf ("%lf\n", result);
+    }
+  } else {
+    printf("Please input a valid operator next time");
+  }
+ 
+  return 0;
+}
